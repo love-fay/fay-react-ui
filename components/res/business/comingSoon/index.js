f3c621f57a8f72736bc8cf500c53071918026067
fay <869770665@qq.com>
2018-09-25 18:58:41 +0800

    update

--- components/res/business/comingSoon/index.js
+++ components/res/business/comingSoon/index.js
@@ -2,8 +2,8 @@
  * Created by feichongzheng on 17/9/27.
  */
 import React from 'react';
-import Loadable  from 'rj-lib/loadable';
+import Loadable  from 'fay-react-lib/loadable';
 
 export default (props) => <Loadable loader={{
-    view: () => import(/* webpackChunkName: "RjdComingSoon" */'./views')
+    view: () => import(/* webpackChunkName: "FayReactUiComingSoon" */'./views')
 }} props={props}/>
